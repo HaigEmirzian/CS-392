dd1d68dbf818eec42be5b2ce10b1883e1c0e905d
Haig Emirzian <hemirzia@stevens.edu>
2023-02-12 18:09:16 -0500

    Update utils.c

--- Assignments/Homework #2/utils.c
+++ Assignments/Homework #2/utils.c
@@ -1,1 +1,37 @@
+#include "utils.h"
 
+// Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+// Name: Haig Emirzian
+
+/*
+	You are free to use any data type you like in this file.
+    However, other than the five functions declared in "utils.h",
+    DO NOT create any other functions.
+	
+*/
+
+int cmpr_int(void*, void*) {
+	
+	/* Your code here */
+}
+
+int cmpr_float(void*,void*) {
+	
+	/* Your code here */
+}
+
+void print_int(void*) {
+	
+	/* Your code here */
+}
+
+void print_float(void*) {
+	
+	/* Your code here */
+}
+
+
+void* read_array(char* filename, char* format, size_t* len) {
+	
+	/* Your code here */
+}
