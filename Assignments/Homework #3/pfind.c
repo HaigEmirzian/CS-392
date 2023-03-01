1be1be2df2a75a3b0bf25d0ba389f3ad38a147ff
HaigEmirzian <hemirzia@stevens.edu>
2023-03-01 16:21:22 -0500

    fixed validation bug

--- Assignments/Homework #3/pfind.c
+++ Assignments/Homework #3/pfind.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <stdbool.h>
+#include <string.h> 
 
 // Pledge: I pledge my honor that I have abided by the Stevens Honor System.
 // Name: Haig Emirzian
@@ -8,10 +11,21 @@
 //checks to see if permission string is valid
 bool validString(const char* permString){
     for(int i = 0; i < 9; i+=3){
-        if((i != "r") && (i != "w") && (i != "x") && (i != "-")){
+        //checks to see if permString is 9
+        if(strlen(permString) != 9){
             fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", permString);
             exit(EXIT_FAILURE);
-        }else if((permString[i] != "r" || permString[i] != "-") && (permString[i+1] != "w" || permString[i] != "-") && (permString[i+2] != "x" || permString[i] != "-")){
+        }
+        //checks to see if either r,w,x,- are being used
+        else if((permString[i] != 'r') && (permString[i] != 'w') && (permString[i] != 'x') && (permString[i] != '-')){
+            fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", permString);
+            exit(EXIT_FAILURE);
+        }
+        //checks to see if rwx are being used in the order they should be in
+        else if(!((permString[i] == 'r' || permString[i] == '-') && 
+                (permString[i+1] == 'w' || permString[i+1] == '-') && 
+                (permString[i+2] == 'x' || permString[i+2] == '-'))){
+
             fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", permString);
             exit(EXIT_FAILURE);
         }
@@ -25,6 +39,7 @@ void navigatingDirectories(const char* directory, const char* permString){
 }
 
 int main(int argc, char* argv[]){
-    
+    char* print = "rwxrwxrwx";
+    validString(print);
     return 0;
 }
