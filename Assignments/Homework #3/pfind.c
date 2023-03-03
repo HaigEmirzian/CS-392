7f4171ac2702b81e6f3c22da13e1749f60c0ae7b
HaigEmirzian <hemirzia@stevens.edu>
2023-03-03 14:29:03 -0500

    update

--- Assignments/Homework #3/pfind.c
+++ Assignments/Homework #3/pfind.c
@@ -4,23 +4,25 @@
 #include <dirent.h>
 #include <stdbool.h>
 #include <string.h> 
+#include <unistd.h>
 
 // Pledge: I pledge my honor that I have abided by the Stevens Honor System.
 // Name: Haig Emirzian
 
 //checks to see if permission string is valid
 bool validString(const char* permString){
+    //checks to see if permString is 9 bits with null terminator
+    if(strlen(permString) != 10){
+        fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", permString);
+        exit(EXIT_FAILURE);
+    }
+
     for(int i = 0; i < 9; i+=3){
-        //checks to see if permString is 9
-        if(strlen(permString) != 9){
-            fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", permString);
-            exit(EXIT_FAILURE);
-        }
         //checks to see if either r,w,x,- are being used
-        else if((permString[i] != 'r') && 
-                (permString[i] != 'w') && 
-                (permString[i] != 'x') && 
-                (permString[i] != '-')){
+        if((permString[i] != 'r') && 
+            (permString[i] != 'w') && 
+            (permString[i] != 'x') && 
+            (permString[i] != '-')){
             
             fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", permString);
             exit(EXIT_FAILURE);
@@ -39,50 +41,75 @@ bool validString(const char* permString){
 
 //recursively navigates directory tree and prints out files that match target perms
 void navigatingFiles(const char* directory, const char* permString){
-    // DIR* dr;
-    // if((dr = opendir(directory)) == NULL){
-    //     char path[100];
-    //     realpath(directory, path);
-    //     fprintf(stderr, "Error: Cannot open directory '%s'. Permission denied.\n", path);
-    //     exit(EXIT_FAILURE);
-    // }
+    DIR* dr;
+    if((dr = opendir(directory)) == NULL){
+        char path[100];
+        realpath(directory, path);
+        fprintf(stderr, "Error: Cannot open directory '%s'. Permission denied.\n", path);
+        exit(EXIT_FAILURE);
+    }
 
-    // //snprintf() to concatenate /home and test_dir /home/test_dir
-    // //get perms of files or directory use stat() stat will give perms in number mode change to rwx format
-    // //use strcmp()
-    // //if statement: if file or dir, if dir then recursive call
+    //snprintf() to concatenate /home and test_dir /home/test_dir
+    //get perms of files or directory use stat() stat will give perms in number mode change to rwx format
+    //use strcmp()
+    //if statement: if file or dir, if dir then recursive call
 
-    // struct stat fileinfo;
-    // char* permBits = "";
-    // int status = stat(directory, &fileinfo);
+    struct stat fileinfo;
+    int status = stat(directory, &fileinfo);
+    char permBits[10] = "";
+    char* r = "r";
+    char* w = "w";
+    char* x = "x";
+    char* - = "-";
 
-    // if((fileinfo.st_mode & S_IRUSR) == 1){
-    //     //strcat(permBits, )
-    // }
+    if((fileinfo.st_mode & S_IRUSR) || (fileinfo.st_mode & S_IRGRP) || (fileinfo.st_mode & S_IROTH)){
+        strcat(permBits, r);
+    } else{
+        strcat(permBits, -);
+    }
 
+    if((fileinfo.st_mode & S_IWUSR) || (fileinfo.st_mode & S_IWGRP) || (fileinfo.st_mode & S_IWOTH)){
+        strcat(permBits, w);
+    } else{
+        strcat(permBits, -);
+    }
 
-    // if(S_ISREG(fileinfo.st_mode)){
+    if((fileinfo.st_mode & S_IXUSR) || (fileinfo.st_mode & S_IXGRP) || (fileinfo.st_mode & S_IXOTH)){
+        strcat(permBits, x);
+    } else{
+        strcat(permBits, -);
+    }
 
-    // } else if(S_ISDIR(fileinfo.st_mode)){
+    strcat(permBits, '\0');
 
-    // }
+    if(S_ISREG(fileinfo.st_mode)){
+        //if reg file, then print file
+    } else if(S_ISDIR(fileinfo.st_mode)){
+        struct dirent* de;
+        while((de = readdir(dr)) != NULL){
+            if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0){
+                continue;
+            }
+            char* newDir = malloc(strlen(directory) + strlen(de->d_name) + 2);
+            snprintf(newDir, strlen(directory) + strlen(de->d_name) + 2, "%s/%s", directory, de->d_name);
+            navigatingFiles(newDir, permString);
+            free(newDir);
+        }
+    }
 
+    closedir(dr);
 }
 
 int main(){
     
+    //tests validString
     char* print = "rwxrwxrwx";
-    
     validString(print);
 
+    //tests navigatingFiles
     char* danger_dir = "test_dir";
     char* permString = "rwxrwxrwx";
     navigatingFiles(danger_dir, permString);
-
-    char* test1 = "Hello ";
-    char* test2 = "World";
-    // strcat(test1, test2);
-    printf("%s\n",test1);
 
     return 0;
 }
